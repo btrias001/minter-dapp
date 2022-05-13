1cca3775f358e8fd1783e0dd23b625d9a105d5d6
btrias001 <btrias001@yahoo.com>
2022-05-14 00:26:34 +0800

    added cntrct address to the opt'l portn in config

--- backend/src/config.js
+++ backend/src/config.js
@@ -69,7 +69,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = ["0xE705c49b7682A364D00a240586FC025b966a6159"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0xba67dBD04Aa09b6E10Aa1BA96c7e74E8a39243E5"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
